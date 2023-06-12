a907864ba7e0c7bdc047ad8d5cacf568c07f12c6
Stuartwastaken <88628337+Stuartwastaken@users.noreply.github.com>
2023-06-12 15:56:30 -0400

    Added a nav bar with components home screen, tab1, tab2

--- lib/navigation/AppNavigator.tsx
+++ lib/navigation/AppNavigator.tsx
@@ -1,54 +1,117 @@
-import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Ionicons } from '@expo/vector-icons';
 
 import HomeScreen, { HomeScreenProps } from '../screens/HomeScreen';
 import LoginScreen from '../screens/loginScreens/LoginScreen';
 import RegisterScreen from '../screens/loginScreens/RegisterScreen';
 import AddNoteScreen from '../screens/AddNoteScreen';
 import EditNote, { EditNoteProps } from '../components/EditNote';
-import { Note } from '../../types';
+import { Note, RootStackParamList } from '../../types';
+import { createStackNavigator } from '@react-navigation/stack';
+import { User } from '../utils/user_class';
 
-export type RootStackParamList = {
+// Get the single instance of the User class
+const user = User.getInstance();
+
+export type RootTabParamList = {
   Home: undefined;
-  Login: undefined;
-  Register: undefined;
-  AddNote: { onSave: (note: Note) => void };
-  EditNote: { note: Note; onSave: (note: Note) => void };
+  Tab1: undefined;
+  Tab2: undefined;
 };
 
+
+
+const Tab = createBottomTabNavigator<RootTabParamList>();
 const Stack = createStackNavigator<RootStackParamList>();
 
+const HomeStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={(props: HomeScreenProps) => <HomeScreen {...props} />}
+        options={{ headerShown: false, gestureEnabled: false }}
+      />
+      <Stack.Screen
+        name="AddNote"
+        component={AddNoteScreen}
+        options={{ headerShown: false, gestureEnabled: false }}
+      />
+      <Stack.Screen
+        name="EditNote"
+        component={(props: EditNoteProps) => <EditNote {...props} />}
+        options={{ headerShown: false, gestureEnabled: false }}
+      />
+    </Stack.Navigator>
+  );
+};
+
 const AppNavigator: React.FC = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(user.getId() !== null);
+
+  // Listen for changes in the user's login state
+  useEffect(() => {
+    // Check the user's login state every second
+    const interval = setInterval(() => {
+      setIsLoggedIn(user.getId() !== null);
+    }, 1000);
+
+    // Clean up the interval on unmount
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen
+      {isLoggedIn ? (
+        <Tab.Navigator
+        screenOptions={{ tabBarShowLabel: false }}
+      >
+        <Tab.Screen
           name="Home"
-          component={(props: HomeScreenProps) => <HomeScreen {...props} />}
-          options={{ headerShown: false, gestureEnabled: false}}
+          component={HomeStack}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="ios-pencil" color={color} size={size} />
+            ),
+          }}
         />
-        <Stack.Screen
-          name="AddNote"
-          component={AddNoteScreen}
-          options={{ headerShown: false, gestureEnabled: false }}
+        <Tab.Screen
+          name="Tab1"
+          component={() => null}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="ios-map" color={color} size={size} />
+            ),
+          }}
         />
-        <Stack.Screen
-          name="EditNote"
-          component={(props: EditNoteProps) => <EditNote {...props} />}
-          options={{headerShown: false, gestureEnabled: false }}
+        <Tab.Screen
+          name="Tab2"
+          component={() => null}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="ios-person" color={color} size={size} />
+            ),
+          }}
         />
-        <Stack.Screen
-          name="Login"
-          component={LoginScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Register"
-          component={RegisterScreen}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
+      </Tab.Navigator>
+      ) : (
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen
+            name="Login"
+            component={LoginScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Register"
+            component={RegisterScreen}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      )}
     </NavigationContainer>
   );
 };
