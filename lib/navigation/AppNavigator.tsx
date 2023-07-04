819a343379fb1bc99020b6e44316010306e1f118
Izak Robles <izakrobles@gmail.com>
2023-07-03 21:17:14 -0500

    error fixed

--- lib/navigation/AppNavigator.tsx
+++ lib/navigation/AppNavigator.tsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from 'react';
 import { Ionicons } from '@expo/vector-icons';
 
 import GoogleMap from '../screens/mapPage/googleMap';
-import ProfilePage from '../screens/profilePage/ProfilePage';
+import ProfilePage from '../screens/profilePage/profilePage';
 import HomeScreen, { HomeScreenProps } from '../screens/HomeScreen';
 import LoginScreen from '../screens/loginScreens/LoginScreen';
 import RegisterScreen from '../screens/loginScreens/RegisterScreen';
