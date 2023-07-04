0a05573c3297a232493f6c6203ab41d2e408f24f
Stuartwastaken <88628337+Stuartwastaken@users.noreply.github.com>
2023-07-03 21:06:57 -0500

    Fixed pathing

--- lib/screens/profilePage/profilePage.tsx
+++ lib/screens/profilePage/profilePage.tsx
@@ -13,7 +13,7 @@ export default function profilePage() {
 
                 <View style={{ alignSelf: "center" }}>
                     <View style={styles.profileImage}>
-                        <Image source={require("../../assets/profile-pic.jpg")} style={styles.image} resizeMode="center"></Image>
+                        <Image source={require("../../../assets/profile-pic.jpg")} style={styles.image} resizeMode="center"></Image>
                     </View>
                     <View style={styles.dm}>
                         <MaterialIcons name="chat" size={18} color="#DFD8C8"></MaterialIcons>
@@ -47,13 +47,13 @@ export default function profilePage() {
                 <View style={{ marginTop: 32 }}>
                     <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
                         <View style={styles.mediaImageContainer}>
-                            <Image source={require("../../assets/media1.jpg")} style={styles.image} resizeMode="cover"></Image>
+                            <Image source={require("../../../assets/media1.jpg")} style={styles.image} resizeMode="cover"></Image>
                         </View>
                         <View style={styles.mediaImageContainer}>
-                            <Image source={require("../../assets/media2.jpg")} style={styles.image} resizeMode="cover"></Image>
+                            <Image source={require("../../../assets/media2.jpg")} style={styles.image} resizeMode="cover"></Image>
                         </View>
                         <View style={styles.mediaImageContainer}>
-                            <Image source={require("../../assets/media3.jpg")} style={styles.image} resizeMode="cover"></Image>
+                            <Image source={require("../../../assets/media3.jpg")} style={styles.image} resizeMode="cover"></Image>
                         </View>
                     </ScrollView>
                     <View style={styles.mediaCount}>
