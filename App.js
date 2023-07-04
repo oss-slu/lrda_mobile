bafea1f1eb2412ff6ab08e1d9187dda7eb9a3744
Stuartwastaken <88628337+Stuartwastaken@users.noreply.github.com>
2023-07-03 20:26:55 -0500

    Prototype google map

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
-import React, { useEffect } from 'react';
-import AppNavigator from './lib/navigation/AppNavigator';
-import { LogBox } from 'react-native';
-LogBox.ignoreAllLogs();
+import React from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
   return (
-    <AppNavigator />
+    <View style={styles.container}>
+      <MapView style={styles.map} />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
---
+++ lib/screens/mapPage/googleMap.tsx
@@ -0,0 +1,21 @@
+// import React from 'react';
+// import MapView from 'react-native-maps';
+// import { StyleSheet, View } from 'react-native';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <MapView style={styles.map} />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//   },
+//   map: {
+//     width: '100%',
+//     height: '100%',
+//   },
+// });
