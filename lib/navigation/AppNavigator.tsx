839234382115059f40850ef3e5a919e7edd7fc43
Stuartwastaken <88628337+Stuartwastaken@users.noreply.github.com>
2023-07-14 11:50:11 -0500

    made paths relative and allowJs true in tsconfig.json

--- lib/navigation/AppNavigator.tsx
+++ lib/navigation/AppNavigator.tsx
@@ -3,7 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import React, { useEffect, useState } from 'react';
 import { Ionicons } from '@expo/vector-icons';
 
-import ExploreScreen from '../screens/mapPage/ExploreScreen';
+import ExploreScreen from '../screens/mapPage/ExploreScreen.js';
 import ProfilePage from '../screens/ProfilePage';
 import HomeScreen, { HomeScreenProps } from '../screens/HomeScreen';
 import LoginScreen from '../screens/loginScreens/LoginScreen';
