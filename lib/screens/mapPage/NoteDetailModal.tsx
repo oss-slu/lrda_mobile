cc0580ea3e5530806a389fda4ff17e012529b929
Stuartwastaken <88628337+Stuartwastaken@users.noreply.github.com>
2023-07-20 14:40:20 -0500

    Made x button an absolute position in top left

--- lib/screens/mapPage/NoteDetailModal.tsx
+++ lib/screens/mapPage/NoteDetailModal.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
-import { ActivityIndicator, ScrollView, View, Text, StyleSheet, Modal, TouchableOpacity, Image } from "react-native";
+import React, { useRef, useState, useEffect } from "react";
+import { ActivityIndicator, ScrollView, View, Text, StyleSheet, Modal, TouchableOpacity, Image, Dimensions } from "react-native";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import { Note } from "../../../types";
 import { Media } from "../../../lib/models/media_class";
+import { format } from 'date-fns'; // Use date-fns or a similar library to format dates nicely
 
 interface Props {
   isVisible: boolean;
@@ -13,10 +14,10 @@ interface Props {
 const NoteDetailModal: React.FC<Props> = ({ isVisible, onClose, note }) => {
   let images: {uri: string}[] = [];
   console.log("This is a NoteDetailModal: ", note);
-  
+
   // Declare a new state variable for image loading
   const [imageLoadedState, setImageLoadedState] = useState<{ [key: string]: boolean }>({});
-  
+
   if (note?.images) {
     images = note.images.filter((mediaItem) => mediaItem.uri.endsWith(".jpg") || mediaItem.uri.endsWith(".png"));
   }
@@ -27,10 +28,10 @@ const NoteDetailModal: React.FC<Props> = ({ isVisible, onClose, note }) => {
 
   return (
     <Modal animationType="slide" transparent={false} visible={isVisible}>
-      <ScrollView contentContainerStyle={styles.modalView}>
-        <TouchableOpacity onPress={onClose}>
-          <Ionicons name="close" size={24} color="#000" />
+      <TouchableOpacity onPress={onClose} style={styles.closeButton}>
+          <Ionicons name="close" size={30} color="#000" />
         </TouchableOpacity>
+      <ScrollView contentContainerStyle={styles.modalView}>
         {images && images.length > 0 ? images.map((image, index) => {
           return (
             <View key={index} style={styles.imageContainer}>
@@ -43,9 +44,11 @@ const NoteDetailModal: React.FC<Props> = ({ isVisible, onClose, note }) => {
             </View>
           )
         }) : <Text>No images</Text>}
+        <View style={styles.textContainer}>
+          <Text style={styles.modalTitle}>{note?.title}</Text>
+          <Text style={styles.modalText}>{note?.description}</Text>
+        </View>
       </ScrollView>
-      <Text style={styles.modalTitle}>{note?.title}</Text>
-      <Text style={styles.modalText}>{note?.description}</Text>
     </Modal>
   );
 };
@@ -55,24 +58,43 @@ const styles = StyleSheet.create({
     marginTop: 50,
     marginLeft: 20,
     marginRight: 20,
-    paddingBottom: 20, // Add padding to ensure all content can be scrolled to
+    paddingBottom: 20,
+  },
+  closeButton: {
+    top: 50,
+    left: 20,
+    zIndex: 1,
+    position: 'absolute',
+  },
+  textContainer: {
+    padding: 20,
+    backgroundColor: '#fafafa',
   },
   modalTitle: {
-    fontSize: 20,
+    fontSize: 24,
+    fontWeight: 'bold',
     marginBottom: 15,
+    color: '#333',
   },
   modalText: {
-    marginBottom: 15,
+    fontSize: 18,
+    lineHeight: 24,
+    marginBottom: 20,
+    color: '#666',
+  },
+  metaData: {
+    fontSize: 14,
+    color: '#888',
   },
   imageContainer: {
-    width: "100%", // Adjust as necessary
-    height: 200, // Adjust as necessary
-    marginBottom: 10, // Adds some space between images
+    width: "100%",
+    height: 200,
+    marginBottom: 10,
   },
   image: {
-    width: "100%", // Adjust as necessary
-    height: 200, // Adjust as necessary
-    resizeMode: "cover", // Or "contain", according to your needs
+    width: "100%",
+    height: 200,
+    resizeMode: "cover",
     position: "absolute",
   },
 });
