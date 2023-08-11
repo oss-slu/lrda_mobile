62c88f70866099dc062e80054ae38674a91492e6
Izak Robles <izakrobles@gmail.com>
2023-08-11 16:53:28 -0500

    global html conversion

--- lib/screens/mapPage/NoteDetailModal.tsx
+++ lib/screens/mapPage/NoteDetailModal.tsx
@@ -1,7 +1,18 @@
 import React, { useState, useEffect } from "react";
-import { ActivityIndicator, ScrollView, View, Text, StyleSheet, Modal, TouchableOpacity, Image } from "react-native";
+import {
+  ActivityIndicator,
+  ScrollView,
+  View,
+  Text,
+  StyleSheet,
+  Modal,
+  TouchableOpacity,
+  Image,
+  useWindowDimensions,
+} from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { Note } from "../../../types";
+import RenderHTML from "react-native-render-html";
 
 interface Props {
   isVisible: boolean;
@@ -10,10 +21,11 @@ interface Props {
 }
 
 const NoteDetailModal: React.FC<Props> = ({ isVisible, onClose, note }) => {
-  let images: {uri: string}[] = [];
-  const [creatorName, setCreatorName] = useState<string>('');
+  let images: { uri: string }[] = [];
+  const [creatorName, setCreatorName] = useState<string>("");
   const [isImageTouched, setImageTouched] = useState(false);
   const [isTextTouched, setTextTouched] = useState(true);
+  const {height, width} = useWindowDimensions();
 
   const handleImageTouchStart = () => {
     setImageTouched(true);
@@ -28,22 +40,29 @@ const NoteDetailModal: React.FC<Props> = ({ isVisible, onClose, note }) => {
   useEffect(() => {
     if (note && note.creator) {
       fetch(note.creator)
-        .then(response => response.json())
-        .then(data => setCreatorName(data.name))
-        .catch(err => console.error('Error fetching creator: ', err));
+        .then((response) => response.json())
+        .then((data) => setCreatorName(data.name))
+        .catch((err) => console.error("Error fetching creator: ", err));
     }
   }, [note]);
 
   // Declare a new state variable for image loading
-  const [imageLoadedState, setImageLoadedState] = useState<{ [key: string]: boolean }>({});
+  const [imageLoadedState, setImageLoadedState] = useState<{
+    [key: string]: boolean;
+  }>({});
 
   if (note?.images) {
-    images = note.images.filter((mediaItem) => mediaItem.uri.endsWith(".jpg") || mediaItem.uri.endsWith(".png"));
+    images = note.images.filter(
+      (mediaItem) =>
+        mediaItem.uri.endsWith(".jpg") || mediaItem.uri.endsWith(".png")
+    );
   }
 
   const handleLoad = (uri: string) => {
-    setImageLoadedState(prev => ({...prev, [uri]: true}));
-  }
+    setImageLoadedState((prev) => ({ ...prev, [uri]: true }));
+  };
+
+  const html = note?.description;
 
   return (
     <Modal animationType="slide" transparent={false} visible={isVisible}>
@@ -52,30 +71,38 @@ const NoteDetailModal: React.FC<Props> = ({ isVisible, onClose, note }) => {
           <Ionicons name="close" size={30} color="#000" />
         </View>
       </TouchableOpacity>
-      
+
       <ScrollView
-        style={{height: isImageTouched ? '80%' : '50%',}}
-        onTouchStart={(images.length  > 2) ? handleImageTouchStart : undefined}
+        style={{ height: isImageTouched ? "80%" : "50%" }}
+        onTouchStart={images.length > 2 ? handleImageTouchStart : undefined}
       >
-        {images && images.length > 0 ? images.map((image, index) => {
-          return (
-            <View key={index} style={styles.imageContainer}>
-              {!imageLoadedState[image.uri] && <ActivityIndicator size="large" color="#0000ff" />}
-              <Image
-                source={{ uri: image.uri }}
-                style={styles.image}
-                onLoad={() => handleLoad(image.uri)}
-              />
-            </View>
-          );
-        }) : <Text style={{alignSelf: 'center', justifyContent: 'center'}}>No images</Text>}
-        <View style={{height:250}}></View>
+        {images && images.length > 0 ? (
+          images.map((image, index) => {
+            return (
+              <View key={index} style={styles.imageContainer}>
+                {!imageLoadedState[image.uri] && (
+                  <ActivityIndicator size="large" color="#0000ff" />
+                )}
+                <Image
+                  source={{ uri: image.uri }}
+                  style={styles.image}
+                  onLoad={() => handleLoad(image.uri)}
+                />
+              </View>
+            );
+          })
+        ) : (
+          <Text style={{ alignSelf: "center", justifyContent: "center" }}>
+            No images
+          </Text>
+        )}
+        <View style={{ height: 250 }}></View>
       </ScrollView>
       <View
         style={[
           styles.textContainer,
           {
-            height: isTextTouched ? '60%' : '30%',
+            height: isTextTouched ? "60%" : "30%",
           },
         ]}
         onTouchStart={handleTextTouchStart}
@@ -83,9 +110,18 @@ const NoteDetailModal: React.FC<Props> = ({ isVisible, onClose, note }) => {
         <ScrollView>
           <Text style={styles.modalTitle}>{note?.title}</Text>
           <Text style={styles.modalText}>{`Created by: ${creatorName}`}</Text>
-          <Text style={[styles.modalText,{marginBottom: 5}]}>{note?.time}</Text>
-          <View style={{height: 2, width: '100%', backgroundColor :'black', marginBottom: 10}}></View>
-          <Text style={styles.modalText}>{note?.description}</Text>
+          <Text style={[styles.modalText, { marginBottom: 5 }]}>
+            {note?.time}
+          </Text>
+          <View
+            style={{
+              height: 2,
+              width: "100%",
+              backgroundColor: "black",
+              marginBottom: 10,
+            }}
+          ></View>
+          <RenderHTML baseStyle={{color: '#666'}} contentWidth={width} source={{ html }} />
         </ScrollView>
       </View>
     </Modal>
@@ -94,49 +130,48 @@ const NoteDetailModal: React.FC<Props> = ({ isVisible, onClose, note }) => {
 
 export default NoteDetailModal;
 
-
 const styles = StyleSheet.create({
   closeButton: {
-    position: 'absolute',
+    position: "absolute",
     top: 50,
     left: 20,
     zIndex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   closeIcon: {
     width: 50,
     height: 50,
     borderRadius: 25,
-    backgroundColor: '#ddd',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ddd",
+    alignItems: "center",
+    justifyContent: "center",
   },
   textContainer: {
     padding: 20,
-    backgroundColor: '#fafafa',
+    backgroundColor: "#fafafa",
     height: 200, // Height of text container
-    position: 'absolute',
+    position: "absolute",
     bottom: 0, // Positioning it at the bottom
     left: 0,
     right: 0,
-    borderTopColor: '#ddd', // Add a border to distinguish from rest of modal
+    borderTopColor: "#ddd", // Add a border to distinguish from rest of modal
     borderTopWidth: 1,
   },
   modalTitle: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 5,
-    color: '#333',
+    color: "#333",
   },
   modalText: {
     fontSize: 18,
     lineHeight: 24,
-    color: '#666',
+    color: "#666",
   },
   metaData: {
     fontSize: 14,
-    color: '#888',
+    color: "#888",
   },
   imageContainer: {
     width: "100%",
