5fe98da7dd2695cbf301738753e25e4c29c42b38
AndrewC <chena9654@parkwayschools.com>
2024-04-18 20:57:07 -0500

    Screen now is adaptable to the dark or light mode of the user

--- lib/screens/mapPage/ExploreLoadingScreen.tsx
+++ lib/screens/mapPage/ExploreLoadingScreen.tsx
@@ -1,11 +1,14 @@
 import { ActivityIndicator, View, Text, StyleSheet } from 'react-native';
+import { useTheme } from '../../components/ThemeProvider';
 
 const ExploreLoadingScreen = () => {
+  const { theme, isDarkmode } = useTheme();
+
   return (
     <View style={styles.centered}>
       <ActivityIndicator size="large" color="#0000ff" />
       <View style={{padding: 5}}></View>
-      <Text style={{ color: 'white' }}>Loading your location...</Text>
+      <Text style={{ color: theme.text }}>Loading your location...</Text>
     </View>
   );
 }
