9d46f2fe056fb59773f21e1ad2984da46337282e
AndrewC <chena9654@parkwayschools.com>
2024-04-18 20:53:09 -0500

    Screen shows up properly now

--- lib/screens/mapPage/ExploreLoadingScreen.tsx
+++ lib/screens/mapPage/ExploreLoadingScreen.tsx
@@ -4,12 +4,12 @@ const ExploreLoadingScreen = () => {
   return (
     <View style={styles.centered}>
       <ActivityIndicator size="large" color="#0000ff" />
-      <Text>Loading your location...</Text>
+      <View style={{padding: 5}}></View>
+      <Text style={{ color: 'white' }}>Loading your location...</Text>
     </View>
   );
 }
 
-// Add this style to your stylesheet
 const styles = StyleSheet.create({
   centered: {
     flex: 1,
