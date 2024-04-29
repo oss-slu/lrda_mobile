6b60bd91809887683079b34f7abd8a7e92bce478
AndrewC <chena9654@parkwayschools.com>
2024-04-29 18:47:43 -0500

    trying to add cookies

--- lib/models/user_class.ts
+++ lib/models/user_class.ts
@@ -60,6 +60,7 @@ export class User {
           method: "POST",
           mode: "cors",
           cache: "no-cache",
+          credentials: "include",
           headers: {
             "Content-Type": "application/json;charset=utf-8",
           },
