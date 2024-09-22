adbd0428adc3f4af88fabaf38172eafb4e03ae8e
AmarHadzic <amar.hadzic@slu.edu>
2024-09-22 13:38:26 -0500

    tests

--- __tests__/LoginScreen.test.tsx
+++ __tests__/LoginScreen.test.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { render } from '@testing-library/react-native';
+import { fireEvent, render, waitFor } from '@testing-library/react-native';
 import { Provider } from 'react-redux';
 import { SafeAreaProvider } from 'react-native-safe-area-context'; // Make sure to import SafeAreaProvider
 import configureStore from 'redux-mock-store';
 import LoginScreen from '../lib/screens/loginScreens/LoginScreen';
 import moxios from 'moxios'
+import { shallow } from 'enzyme';
 // Create a mock store
 const mockStore = configureStore([]);
 const store = mockStore({
@@ -45,4 +46,34 @@ describe('LoginScreen', () => {
 
     expect(toJSON()).toMatchSnapshot();
   });
+
+  it('triggers login function when login button is pressed while keyboard is mounted', async () => {
+    const navigationMock = { navigate: jest.fn() };
+    const routeMock = { params: {} };
+
+    const { getByPlaceholderText, getByText } = render(
+      <Provider store={store}>
+        <SafeAreaProvider>
+          <LoginScreen navigation={navigationMock} route={routeMock} />
+        </SafeAreaProvider>
+      </Provider>
+    );
+    
+    // Simulate user input for email and password
+    const emailInput = await waitFor(() => getByPlaceholderText('Email...'));  // Wait for the email input to appear
+    fireEvent.changeText(emailInput, 'testuser@example.com');
+    
+    const passwordInput = await waitFor(() => getByPlaceholderText('Password...'));  // Wait for the password input to appear
+    fireEvent.changeText(passwordInput, 'password123');
+
+    // Simulate pressing the login button
+    const loginButton = getByText('Login');
+    fireEvent.press(loginButton);
+
+    // Assert that the navigation function was called with "HomeTab"
+    await waitFor(() => {
+      expect(navigationMock.navigate).toHaveBeenCalledWith('HomeTab');
+    });
+  });
+  
 });
