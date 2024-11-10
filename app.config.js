61555c5557264b1c10f9a7a464d2aedb3cfeec69
Yash Kamal Bhatia <113057400+yashb196@users.noreply.github.com>
2024-11-10 01:17:01 -0600

    trying to see why it is crashing

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: "Where's Religion?",
     slug: "lrda_mobile",
-    version: "1.0.5",
+    version: "1.0.4",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
@@ -20,7 +20,7 @@ export default {
       config: {
         googleMapsApiKey: process.env.MAP_API_KEY
       },
-      buildNumber: "19"
+      buildNumber: "2"
     },
     android: {
       adaptiveIcon: {
@@ -42,20 +42,21 @@ export default {
       policy: "sdkVersion"
     },
     updates: {
-      url: "https://u.expo.dev/801029ef-db83-4668-a97a-5adcc4c333e2"
+      "url": "https://u.expo.dev/801029ef-db83-4668-a97a-5adcc4c333e2"
     },
-    crashReporter: true,
+    crashReporter: true, // Add this line to enable crash reporting
     extra: {
-      MAP_API_KEY: process.env.MAP_API_KEY,
-      FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
-      AUTH_DOMAIN: process.env.AUTH_DOMAIN,
-      PROJECT_ID: process.env.PROJECT_ID,
-      STORAGE_BUCKET: process.env.STORAGE_BUCKET,
-      MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID,
-      APP_ID: process.env.APP_ID,
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      projectId: process.env.PROJECT_ID,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID,
+      appId: process.env.APP_ID,
+      measurementId: process.env.MEASUREMENT_ID,
       eas: {
-        projectId: "801029ef-db83-4668-a97a-5adcc4c333e2"
+        "projectId": "801029ef-db83-4668-a97a-5adcc4c333e2"
       }
     },
+   
   }
 };
