0e82a82bd37441331cfaa0078fd31d1f99a67b21
Yash Kamal Bhatia <113057400+yashb196@users.noreply.github.com>
2024-11-10 00:38:34 -0600

    trying to get a version ready

--- app.config.js
+++ app.config.js
@@ -42,21 +42,20 @@ export default {
       policy: "sdkVersion"
     },
     updates: {
-      "url": "https://u.expo.dev/801029ef-db83-4668-a97a-5adcc4c333e2"
+      url: "https://u.expo.dev/801029ef-db83-4668-a97a-5adcc4c333e2"
     },
-    crashReporter: true, // Add this line to enable crash reporting
+    crashReporter: true,
     extra: {
-      apiKey: process.env.FIREBASE_API_KEY,
-      authDomain: process.env.AUTH_DOMAIN,
-      projectId: process.env.PROJECT_ID,
-      storageBucket: process.env.STORAGE_BUCKET,
-      messagingSenderId: process.env.MESSAGING_SENDER_ID,
-      appId: process.env.APP_ID,
-      measurementId: process.env.MEASUREMENT_ID,
+      MAP_API_KEY: process.env.MAP_API_KEY,
+      FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+      AUTH_DOMAIN: process.env.AUTH_DOMAIN,
+      PROJECT_ID: process.env.PROJECT_ID,
+      STORAGE_BUCKET: process.env.STORAGE_BUCKET,
+      MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID,
+      APP_ID: process.env.APP_ID,
       eas: {
-        "projectId": "801029ef-db83-4668-a97a-5adcc4c333e2"
+        projectId: "801029ef-db83-4668-a97a-5adcc4c333e2"
       }
     },
-   
   }
 };
